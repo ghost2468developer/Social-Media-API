65034bb66ae8f038fe9b8d53d8dcc085e5dbfc2e
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2026-03-11 15:22:13 +0200

    feat: set up API

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+import authRoutes from './routes/auth.js'
+import postRoutes from './routes/posts.js'
+import commentRoutes from './routes/comments.js'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/comments', commentRoutes)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
